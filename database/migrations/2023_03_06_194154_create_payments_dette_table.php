4aa5779464673401080f6513d0deebb7af3f9b38
Wissam Kechkar <wissam.kechkar@gmail.com>
2023-03-07 14:01:24 +0100

    first commit

---
+++ database/migrations/2023_03_06_194154_create_payments_dette_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments_dette', function (Blueprint $table) {
+            $table->id();
+            $table->double('amount');
+            $table->string('payment_method');
+            $table->string('status');
+            $table->unsignedBigInteger('seller_id');
+            $table->foreign('seller_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('buyer_id');
+            $table->foreign('buyer_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments_dette');
+    }
+};
